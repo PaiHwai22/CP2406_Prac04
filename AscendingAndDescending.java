d7686fc535f68957fac06b083b1e8c5028906cdb
PaiHwai22/Pai22 <pai.hwai@my.jcu.edu.au>
2023-01-15 17:50:38 +0700

    Reduce noise.

--- AscendingAndDescending.java
+++ AscendingAndDescending.java
@@ -1,14 +1,12 @@
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
 import java.util.*;
-
-import static java.util.Collections.*;
+/* This class gets user to input 3 integers and prints them in ascending and descending order. */
 
 public class AscendingAndDescending{
     public static void main(String[] args) {
 
-        Scanner sc= new Scanner(System.in);    //System.in is a standard input stream
+        Scanner sc= new Scanner(System.in);
         System.out.print("Enter first integer:  ");
         int a= sc.nextInt();
         System.out.print("Enter second integer:  ");
@@ -19,11 +17,9 @@ public class AscendingAndDescending{
         List< Integer > list = new ArrayList<>(List.of(a, b, c));
 
         Collections.sort(list);
-        System.out.print("\nAscending order: " + list);
-
-        System.out.println("\nDescending order: ");
+        System.out.print("\nAscending order: \n" + list);
         list.sort(Collections.reverseOrder());
-        System.out.println( list );
+        System.out.println("\nDescending order: " + list);
 
     }
 
